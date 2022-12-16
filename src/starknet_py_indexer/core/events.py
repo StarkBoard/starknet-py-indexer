12531dff4afdbfd1c2e93adb4b0ffdbe83b77fbb
Exo <0xexomonk@gmail.com>
2022-12-16 16:44:45 +0100

    Adding Code Documentation

--- src/starknet_py_indexer/core/events.py
+++ src/starknet_py_indexer/core/events.py
@@ -2,6 +2,13 @@ import json
 
 def events_in_block(block_id, provider):
     """Retrieve the list of events in a given block
+
+    Parameters
+    ----------
+    block_id: BlockId
+        block identifier, "latest" or object {"block_number": 1}
+    provider: starknet_py_indexer.Provider
+        jsonRPC Provider Instance
     """
     params = {
         "filter": {
@@ -25,3 +32,16 @@ def events_in_block(block_id, provider):
         data = data["result"]
         events += data["events"]
     return events
+
+def filter_events(events, keys):
+    """Filtering events of a given key
+
+    Parameters
+    ----------
+    events: list
+        list of events to filter on
+    keys: list
+        list of string filter
+    """
+    filtered_events = list(filter(lambda event: event['keys'][0] in keys, events))
+    return filtered_events
